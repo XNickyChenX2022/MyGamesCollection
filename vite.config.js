5dfc62bbb96f8ba94a0248116de89ea6a7ff82d2
Nicky Chen <nickychen2022@gmail.com>
2023-08-02 00:30:43 -0400

    changed file private to false

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "Rating-App-Frontend",
   server: {
     port: 3000,
     proxy: {
