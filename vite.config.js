78ac3301869cb5a8892e254925e998a828b03b0b
Nicky Chen <nickychen2022@gmail.com>
2023-08-02 01:03:53 -0400

    added base url for imports for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "Rating-App-Frontend",
+  base: "/Rating-App-Frontend/",
   server: {
     port: 3000,
     proxy: {
