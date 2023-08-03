067b85eeb29d4b5f37cbbccc18c8fe181ccaa354
Nicky Chen <nickychen2022@gmail.com>
2023-08-02 22:22:49 -0400

    configured cors to allow only specific url and allowed to pass credentials

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   base: "/MyGamesCollection/",
   server: {
     port: 3000,
-    proxy: {
-      "/api": {
-        target: "https://rating-app-backend.onrender.com/",
-        changeOrigin: true,
-      },
-    },
+    // proxy: {
+    //   "/api": {
+    //     target: "https://rating-app-backend.onrender.com/",
+    //     changeOrigin: true,
+    //   },
+    // },
   },
 });
